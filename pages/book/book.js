a5c3a684777dddad10d57c70074eb0290c57dc5f
ITxiaohao <shunhaozeng@gmail.com>
2018-10-12 16:39:27 +0800

    :sparkles:添加图标，初步完成书籍页面布局和样式，以及循环v-book组件

--- pages/book/book.js
+++ pages/book/book.js
@@ -7,35 +7,19 @@ Page({
   /**
    * 页面的初始数据
    */
-  data: {},
+  data: {
+    books: []
+  },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
     bookModel.getHotList().then(res => {
-      console.log(res)
+      this.setData({
+        books: res
+      })
     })
-
-    // const promise = new Promise((reslove, reject) => {
-    //   // promise 的三种状态，pending fulfilled rejected 进行中，已成功，已失败，一旦我们 new promise 就在 pending 状态
-    //   // 如何把一个进行中的 promise 修改为一个已成功或者已失败的状态呢？通过 reslove 和 reject
-    //   // 一旦修改了已成功或者已失败，promise 的状态就凝固了，不能再改变
-    //   wx.getSystemInfo({
-    //     success: res => reslove(res),
-    //     fail: error => reject(error)
-    //   })
-    // })
-
-    // // 用 promise 的变量来获取异步回调的结果, then 方法接收两个回调函数，第一个是已成功的结果，第二个是已失败
-    // promise.then(
-    //   res => {
-    //     console.log(res)
-    //   },
-    //   error => {
-    //     console.log(error)
-    //   }
-    // )
   },
 
   /**
